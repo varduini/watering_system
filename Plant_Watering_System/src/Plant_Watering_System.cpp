e16aad18c5d7db1be385e32c8d9c153cd9f32b95
Viola Arduini <violaarduini@Violas-MBP.admin.ad.cnm.edu>
2023-11-09 16:38:14 -0700

    final code

--- Plant_Watering_System/src/Plant_Watering_System.cpp
+++ Plant_Watering_System/src/Plant_Watering_System.cpp
@@ -20,9 +20,9 @@
 
 
 // Let Device OS manage the connection to the Particle Cloud
-SYSTEM_MODE(SEMI_AUTOMATIC);
-#define OLED_RESET D4
-Adafruit_SSD1306 display (OLED_RESET);
+SYSTEM_MODE(AUTOMATIC);
+//#define OLED_RESET D4
+Adafruit_SSD1306 display (-1);
 Adafruit_BME280 bme;
 TCPClient TheClient; 
 Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
@@ -32,8 +32,8 @@ Adafruit_MQTT_Publish soilMoistureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNA
 Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperaturefeed");
 Adafruit_MQTT_Publish pressureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/pressurerefeed");
 Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidityfeed");
-Adafruit_MQTT_Subscribe waterFeed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/waterbutton"); 
-
+Adafruit_MQTT_Subscribe waterButton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/waterbutton"); 
+Adafruit_MQTT_Publish airQualityFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/airqualityfeed");
 
 // Run the application and system concurrently in separate threads
 //SYSTEM_THREAD(ENABLED);
@@ -49,13 +49,13 @@ int lastTime;
 const int DUSTPIN = D6;
 int duration;
 int lastRead;
-int lowpulseoccupancy = 0;
+
 int sampletime_ms = 30000;
 float ratio;
 float concentration;
 bool status;
 float subValue;
-
+int dustTimer;
 
 AirQualitySensor airSensor (A5);
 
@@ -65,6 +65,7 @@ void dataBME ();
 int soilMoisture ();
 void MQTT_connect();
 bool MQTT_ping();
+float getDust (int DUSTPIN);
 
 // setup() runs once, when the device is first turned on
 void setup() {
@@ -72,6 +73,8 @@ void setup() {
   pinMode (PUMPPIN, OUTPUT);
   pinMode (SOILPIN, INPUT);
   
+  Wire.begin(); 
+
   Serial.begin (9600);
   waitFor (Serial.isConnected,10000);
 
@@ -83,11 +86,13 @@ void setup() {
 
   airSensor.init ();
 
+  mqtt.subscribe(&waterButton);
   pinMode(DUSTPIN,INPUT);
+
     //startTime = millis();
     //lastTime= -1000000;
 
-  status=bme.begin (0x76);
+  status=bme.begin(0x76);
     delay(2000);
     if (status==false) {
         Serial.printf ("BME280 at address 0x76 X failed to start" );
@@ -108,51 +113,61 @@ void loop() {
   MQTT_connect();
   MQTT_ping();
 
-// Adafruit_MQTT_Subscribe *subscription;
-//   while ((subscription = mqtt.readSubscription(2000))) {
-//     if (subscription == &waterFeed) {
-//       subValue = atof((char *)waterFeed.lastread);
-//       if (subValue == 1){
-//         while ((millis()-lastTime > 500)){
-//       digitalWrite (PUMPPIN, HIGH);
-//       Serial.printf ("Watering the plant\n");
-//       }
-//         }
-//     }
-//   }
-duration = pulseIn(DUSTPIN, LOW);
-    lowpulseoccupancy = lowpulseoccupancy+duration;
+Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(100))) {
+    if (subscription == &waterButton) {
+      subValue = atof((char *)waterButton.lastread);
+      //Serial.printf("got value\n");
+      if (subValue == 1){
+      digitalWrite (PUMPPIN, HIGH);
+      Serial.printf ("Watering the plant\n");
+      delay (500);
+      digitalWrite (PUMPPIN, LOW);
+      }
+        }
+    }
 
-while ((millis()-lastRead) > 30000) //if the sampel time == 30s
-{    
+if ((millis()-dustTimer)>300000) { 
+
+airSensing ();
+//Serial.printf ("Air Quality %i\n", airSensing ());
+dataBME ();
+//soilMoisture ();
+Serial.printf ("Soil moisture is %i\n", soilMoisture());
+concentration= getDust (DUSTPIN);
+Serial.printf ("concentration= %0.2f\n", concentration);
+   if(mqtt.Update()) {
+    soilMoistureFeed.publish(soilMoisture());}
+    dustTimer=millis();
+}
+//       }
+//lastTime = millis ();
+
+    }
+
+
+float getDust(int DUSTPIN) {
+  float concentration;
+  int duration;
+  int lowpulseoccupancy;
+  unsigned int startTime;
+
+  lowpulseoccupancy = 0;
+ 
+  while ((millis()-startTime)<30000) {
+    duration = pulseIn(DUSTPIN, LOW);
+    lowpulseoccupancy = lowpulseoccupancy+duration;
+  
+  }
+
    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
         concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
-    if ((millis()-lastTime > 300000)) {   
-      Serial.printf ("lowpulseoccupancy=%i\n", lowpulseoccupancy);
-        Serial.printf ("ratio=%f \n", ratio);
-        Serial.printf ("concentration= %f\n", concentration);
-        lowpulseoccupancy = 0;
-        lastRead = millis();
-    
-
- airSensing ();
-Serial.printf ("Air Quality %i\n", airSensing ());
-dataBME ();
-soilMoisture ();
-Serial.printf ("Soil moisture is %i\n", soilMoisture ());
-   if(mqtt.Update()) {
-    soilMoistureFeed.publish(soilMoisture ());
-
-      } 
-
-
-
-lastTime = millis ();
-    }
-  }
+        startTime = millis ();
+    return concentration;
 }
 
-void dataBME () {
+
+void dataBME() {
   float tempC;
   float tempF;
   int pressPA;
@@ -166,44 +181,86 @@ void dataBME () {
 
   Serial.printf ("Temperature= %f\n Pressure=%i\n Humidity=%i\n", tempC, hgmm, humidRH);
      if(mqtt.Update()) {
-    temperatureFeed.publish(tempC);}
+    temperatureFeed.publish(tempC);
     pressureFeed.publish(hgmm);
     humidityFeed.publish (humidRH);
+  }
 }
 
-int airSensing () {
+int airSensing() {
   int pollution;
 int quality = airSensor.slope();
-     {
+
         if (quality == AirQualitySensor::FORCE_SIGNAL) {
-            Serial.printf("High pollution! Force signal active! \n");}
+            Serial.printf("High pollution! Force signal active! \n");
+            }
         else if (quality == AirQualitySensor::HIGH_POLLUTION) {
-            Serial.printf("High pollution!\n");}
+            Serial.printf("High pollution!\n");
+            }
         else if (quality == AirQualitySensor::LOW_POLLUTION) {
-            Serial.printf("Low pollution!\n"); }
+            Serial.printf("Low pollution!\n");
+            }
         else if (quality == AirQualitySensor::FRESH_AIR) {
-            Serial.printf("Fresh air\n"); }
-    } 
+            Serial.printf("Fresh air\n");
+            }
+    
+  
+  if(mqtt.Update()) {
+    airQualityFeed.publish(quality);
+
+      } 
   pollution=airSensor.slope();
   return pollution;
 }
 
-int soilMoisture (){
+int soilMoisture(){
   int moisture;
 
  moisture= analogRead(SOILPIN);
 
+ //Serial.printf ("Soil moisture is %i\n", soilMoisture ());
+   if(mqtt.Update()) {
+    soilMoistureFeed.publish(moisture);}
+
  if (moisture<2000) {
-     while ((millis()-lastTime < 500)) {  
-      digitalWrite (PUMPPIN, HIGH); }
-      lastTime = millis ();
+     //while ((millis()-lastTime < 500)) {  
+      digitalWrite (PUMPPIN, HIGH);
+       delay (500); 
+      //}
+      //lastTime = millis ();
       digitalWrite (PUMPPIN, LOW);
-  //OLED talks
+  //publish soil levels
+
+      
+  // DateTime= Time.timeStr (); // Current Date and Time from Particle Time class
+  // TimeOnly= DateTime.substring (11 ,19) ;
+
+display.clearDisplay();
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor (0,5);
+display.printf ("moisture=%i \n, watering plant\n", moisture);
+display.display();
+delay (2000);
+display.clearDisplay();
+ }
+
+ else {
+  display.clearDisplay();
+
+display.setTextSize(2);
+display.setTextColor(WHITE);
+display.setCursor (0,5);
+display.printf ("optimal soil moisture \n");
+display.display();
+delay (2000);
+display.clearDisplay();
  }
 
  return moisture;
 
 }
+
 
 void MQTT_connect() {
   int8_t ret;
@@ -239,17 +296,3 @@ bool MQTT_ping() {
   }
   return pingStatus;
 }
-
-//publish soil levels
-//   DateTime= Time.timeStr (); // Current Date and Time from Particle Time class
-//   TimeOnly= DateTime.substring (11 ,19) ;
-
-// display.clearDisplay();
-
-//   display.setTextSize(2);
-//   display.setTextColor(WHITE);
-//   display.setCursor (0,5);
-//   display.printf ("%i \n %s \n", moisture, TimeOnly.c_str());
-//   display.display();
-//   delay (2000);
-
